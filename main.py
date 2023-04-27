abab62103be028eccc14bfdf9a31713bf5f73c51
dwd <wonder9916@gmail.com>
2023-04-27 15:07:28 +0900

    lotto second commit

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
+import tkinter              # 그래픽 tkinter 가져오기
+import tkinter.font         # tkinter의 font메소드 가져오기
 import random
 
 lotto_num = range(1, 46)
 
-for i in range(5):
+def buttonClick():
     print(random.sample(lotto_num, 6))
+
+window=tkinter.Tk()
+window.title("lotto")
+window.geometry("400x200")
+window.resizable(False, False)
+
+button = tkinter.Button(window, overrelief="solid", text="번호 확인", width=15,
+                        command=buttonClick, repeatdelay=1000, repeatinterval=100)
+button.pack()
+
+window.mainloop()
